a21433f92c9f6c3767ee9248f44ff21772b46e9e
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2025-02-12 21:23:31 -0600

    combobox.tsx added

--- src/app/dentist/page.tsx
+++ src/app/dentist/page.tsx
@@ -1,26 +1,14 @@
 "use client";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import AddDentist from "./Agregar";
-export default function Home({ children }: { children: React.ReactNode }) {
+import Dentist from "./Dentist";
 
-    const router = useRouter();
+export default function Home() {
 
-    useEffect(() => {
-
-        const token = sessionStorage.getItem('token')
-        const email = sessionStorage.getItem('user_email')
-
-        if(token === null || email === null){
-            router.push('/Login')
-        }
-
-    }, []) 
-
+ 
     return (
+        
         <>
-            <AddDentist></AddDentist>
-            
+            <Dentist></Dentist>
         </>
+
     )
 }
